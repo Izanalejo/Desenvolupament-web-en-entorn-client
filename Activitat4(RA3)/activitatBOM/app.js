d9ebce765cad2779ca6e3f444bdcb655cd421a9b
Izan Alejo <izanap2006@gmail.com>
2025-11-24 12:56:51 +0100

    Ejs BOM

---
+++ Activitat4(RA3)/activitatBOM/app.js
@@ -0,0 +1,15 @@
+// ---------- SALUTACIÓ BASADA EN COOKIES ----------
+
+// ---------- 1. INFORMACIÓ DEL NAVEGADOR ----------
+
+// ---------- 2. MIDA DE LA FINESTRA EN TEMPS REAL ----------
+
+// ---------- 3. COMPTADOR + REDIRECCIÓ ----------
+
+// ---------- 4. HISTÒRIA ----------
+
+// ---------- 5. FINESTRES / PESTANYES ----------
+
+// ---------- 6. COOKIES ----------
+
+// ---------- 7.  EXTRA. REPÀS DE DOM: CANVI DE COLOR RANDOM I RECOMPTE D'ENTRADES A CADA SESSIÓ ----------
