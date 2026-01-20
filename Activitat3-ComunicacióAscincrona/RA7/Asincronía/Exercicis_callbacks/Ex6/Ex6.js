2a2e53522da32bb88fb1169d81c6bc4c1fb5d03d
Izan <Izanap2006@gmail.com>
2026-01-20 21:04:59 +0100

    Ejs callbacks 2.0

--- Activitat3-ComunicacióAscincrona/RA7/Asincronía/Exercicis_callbacks/Ex6/Ex6.js
+++ Activitat3-ComunicacióAscincrona/RA7/Asincronía/Exercicis_callbacks/Ex6/Ex6.js
@@ -13,12 +13,30 @@ El que passa després de la càrrega s’ha de fer mitjançant un callback
 
 let btnUser = document.getElementById('btnUser');
 
-
-
 function carregarUsuari(){
-    console.log("Carregant...");
+    let div = document.createElement('div');
+    let titulo = document.createElement('h4');
+    titulo.textContent = "Carregant...";
+    document.body.appendChild(titulo);
+    setTimeout(() => {
+    titulo.style.color = "green";
+    }, 2000);
+}
+function task2(callback){
+    setTimeout(() => {
+    let div = document.createElement('div');
+    let titulo = document.createElement('p');
+    titulo.textContent = "Nom de l'usuari: Izan";
+    document.body.appendChild(titulo);
+    }, 2000);
 }
 
 btnUser.addEventListener('click', () => {
-
+    document.getElementById("btnUser").disabled = true;
+    carregarUsuari();
+    task2();
+    setTimeout(() =>{
+    document.getElementById("btnUser").disabled = false;
+    }, 2000);
+    
 });
